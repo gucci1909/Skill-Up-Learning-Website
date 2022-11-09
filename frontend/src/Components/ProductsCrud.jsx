fd227ef451f44efa13bd1e612fb675c13076a048
gucci1909 <umangarora0134@gmail.com>
2022-11-09 20:30:29 +0530

    frontend

---
+++ frontend/src/Components/ProductsCrud.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsCrud() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsCrud
---
+++ frontend/src/Components/ProductsGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsGrid() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsGrid

6f3ee52a84372a6aad906e7e7b6f65a87862f228
unknown <NazminS28>
2022-11-10 16:27:24 +0530

    Navbar

--- Sanjeev_Singh/sanju/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {legacy_createStore,combineReducers,applyMiddleware} from "redux"
-import thunk from "redux-thunk";
-import { productReducer } from "./prod.reducer";
-const rootReducer=combineReducers({
-    product:productReducer,
-})
-export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))

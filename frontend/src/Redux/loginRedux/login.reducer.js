9574dcb1727cabc6951d0a138f6427690a79343a
gucci1909 <umangarora0134@gmail.com>
2022-11-11 23:07:51 +0530

    frontend

---
+++ frontend/src/Redux/loginRedux/login.reducer.js
@@ -0,0 +1,1 @@
+export const loginReducer = "abc"
---
+++ frontend/src/Redux/store.js
@@ -0,0 +1,13 @@
+import { legacy_createStore, combineReducers,applyMiddleware,compose } from "redux";
+import thunk from "redux-thunk"
+import { loginReducer } from "./loginRedux/login.reducer";
+import { productReducer } from "./productsRedux/products.reducer";
+
+const rootReducer = combineReducers({
+    login : loginReducer,
+    provider : productReducer
+})
+
+const createComposer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(rootReducer, createComposer(applyMiddleware(thunk)))

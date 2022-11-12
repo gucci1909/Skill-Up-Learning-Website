100dd15e73501ef3ec74622f620fdbd7c0e243f0
Umang Arora <104164939+gucci1909@users.noreply.github.com>
2022-11-12 20:18:14 +0530

    Delete Sanjeev_Singh/sanju directory

--- Sanjeev_Singh/sanju/src/redux/product/prod.actionType.js
+++
@@ -1,3 +0,0 @@
-export const GET_PRODUCTS_LOADING = 'products/loading';
-export const GET_PRODUCTS_SUCCESS = 'products/success';
-export const GET_PRODUCTS_ERROR = 'products/error';
--- Sanjeev_Singh/sanju/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {legacy_createStore,combineReducers,applyMiddleware} from "redux"
-import thunk from "redux-thunk";
-import { productReducer } from "./product/prod.reducer";
-const rootReducer=combineReducers({
-    product:productReducer,
-})
-export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))

e5d32d0547c801805d3b60f97432ed9056be173a
gucci1909 <umangarora0134@gmail.com>
2022-11-12 21:05:08 +0530

    frontend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, CloseButton, Flex, Heading, Image, Input, Spinner} from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  CloseButton,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  Spinner,
+} from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { post_login } from "../Redux/loginRedux/login.actions";
@@ -30,89 +43,103 @@ function Login() {
     return dispatch(post_login(login_credentials));
   };
   const handleError = () => {
-    dispatch({type:ERROR_HANDLING})
-  }
+    dispatch({ type: ERROR_HANDLING });
+  };
   if (login.error) {
-   return <Alert status='error' h="100px" w="450px" ml="510px">
-    <AlertIcon />
-    <Box>
-      <AlertTitle>ERROR HANDLING YOUR REQUEST!</AlertTitle>
-      <AlertDescription>
-       Please Write Right Credentials in your Login Details
-      </AlertDescription>
-    </Box>
-    <CloseButton
-      alignSelf='flex-start'
-      position='relative'
-      right={-1}
-      top={-1}
-      onClick={()=>handleError()}
-    />
-  </Alert>
+    return (
+      <Alert status="error" h="100px" w="450px" ml="510px">
+        <AlertIcon />
+        <Box>
+          <AlertTitle>ERROR HANDLING YOUR REQUEST!</AlertTitle>
+          <AlertDescription>
+            Please Write Right Credentials in your Login Details
+          </AlertDescription>
+        </Box>
+        <CloseButton
+          alignSelf="flex-start"
+          position="relative"
+          right={-1}
+          top={-1}
+          onClick={() => handleError()}
+        />
+      </Alert>
+    );
   }
   if (login.isAuth) {
     return <Navigate to="/productsAdmin" />;
   }
   return (
     <Box
-    display= 'flex'
-    height='716px'
-    backgroundPosition='center'
-    backgroundRepeat= 'no-repeat'
-    backgroundSize= 'cover'
-     bgImage="url('https://habitatbroward.org/wp-content/uploads/2020/01/10-Benefits-Showing-Why-Education-Is-Important-to-Our-Society.jpg')">
+      display="flex"
+      height="716px"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      bgImage="url('https://habitatbroward.org/wp-content/uploads/2020/01/10-Benefits-Showing-Why-Education-Is-Important-to-Our-Society.jpg')"
+    >
       <Heading
-      fontSize= '55px'
-      fontWeight= '200'
-      position="absolute"
-      mt="140px"
-      ml="585px"
-
-      color = 'linear-gradient( #553c9a, #ee4b2b);'
-      background-clip= 'text'>Admin Login</Heading>
-      <Flex mt="250px" flexDirection={'column'} gap="30px"  height="100px" w="500px" ml={'480px'}  justify="center" alignItems={'center'}>
-
-      <Input
-        type="text"
-        bgColor={'#EBF8FF'}
-        placeholder="Admin Username"
-        value={login_cred.username}
-        name="username"
-        onChange={(e) => handleChange(e)}
-      />
-      <Input
-       bgColor={'#EBF8FF'}
-        type="password"
-        placeholder="Admin Password"
-        value={login_cred.password}
-        name="password"
-        onChange={(e) => handleChange(e)}
-      />
+        fontSize="55px"
+        fontWeight="200"
+        position="absolute"
+        mt="140px"
+        ml="585px"
+        color="linear-gradient( #553c9a, #ee4b2b);"
+        background-clip="text"
+      >
+        Admin Login
+      </Heading>
+      <Flex
+        mt="250px"
+        flexDirection={"column"}
+        gap="30px"
+        height="100px"
+        w="500px"
+        ml={"480px"}
+        justify="center"
+        alignItems={"center"}
+      >
+        <Input
+          type="text"
+          bgColor={"#EBF8FF"}
+          placeholder="Admin Username"
+          value={login_cred.username}
+          name="username"
+          onChange={(e) => handleChange(e)}
+        />
+        <Input
+          bgColor={"#EBF8FF"}
+          type="password"
+          placeholder="Admin Password"
+          value={login_cred.password}
+          name="password"
+          onChange={(e) => handleChange(e)}
+        />
       </Flex>
       {login.loading ? (
-        <Box display={'flex'} flexDirection='column' mt={"390px"} ml={"-280px"}>
-          <Spinner 
-          hickness='4px'
-          speed='0.65s'
-          emptyColor='gray.200'
-          color='blue.500'
-          size='xl'/>
+        <Box display={"flex"} flexDirection="column" mt={"390px"} ml={"-280px"}>
+          <Spinner
+            hickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
         </Box>
-          
       ) : (
-        <Box display={'flex'} flexDirection='column' mt={"365px"} ml={"-300px"}>
-        <Button 
-        mt="30px"
-        colorScheme={"red"}
-          onClick={() => handleClick(login_cred.username, login_cred.password)}
-        >
-          Login
-        </Button>
-
+        <Box display={"flex"} flexDirection="column" mt={"365px"} ml={"-300px"}>
+          <Button
+            mt="30px"
+            colorScheme={"red"}
+            onClick={() =>
+              handleClick(login_cred.username, login_cred.password)
+            }
+          >
+            Login
+          </Button>
         </Box>
       )}
     </Box>
-  )
+  );
 }
 
 export default Login;
--- frontend/src/Components/ProductsCrud.jsx
+++ frontend/src/Components/ProductsCrud.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Box, Button, Heading, Image, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Heading, Image, Input, Skeleton, Text, Textarea, VStack } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   deleteProduct,
   getProduct,
   patchProduct,
-  postProduct
+  postProduct,
 } from "../Redux/productsRedux/products.actions";
 
 function ProductsCrud() {
@@ -26,17 +26,18 @@ function ProductsCrud() {
       ...products,
       [name]: value,
     });
+
   };
-  const handleGet = ()=>{
+  const handleGet = () => {
     dispatch(getProduct());
-  }
-  const handleEdit = (id) => {
+  };
+  const handleEdit = (id,price) => {
     const new_price = prompt("New Price");
     setTimeout(function () {
-      handleEditPrice(new_price, id);
+      handleEditPrice(new_price, id,price);
     }, 2000);
   };
-  const handleEditPrice = (price, id) => {
+  const handleEditPrice = (price, id,price1) => {
     if (price.length === 0) {
       alert("Write Something in Edit");
     } else {
@@ -45,83 +46,244 @@ function ProductsCrud() {
       };
       setTimeout(function () {
         handleGet();
+        alert(`Price has been changed from ${price1} to ${price}`);
       }, 2000);
       dispatch(patchProduct(id, value));
     }
-  }
+  };
   const handleDelete = (id) => {
     setTimeout(function () {
       handleGet();
+      alert("Deleted Successfully");
     }, 2000);
     dispatch(deleteProduct(id));
   };
   const handlePost = (value) => {
     setTimeout(function () {
       handleGet();
+      alert("Posted Successfully");
     }, 2000);
     dispatch(postProduct(value));
+    setProducts({
+      ...products,
+      course_name:"",
+      description:"",
+      image:"",
+      price:0,
+      discount:""
+    })
   };
   useEffect(() => {
     dispatch(getProduct());
   }, [dispatch]);
-  if(error){
+  if (error) {
     alert("Sorry your Request is giving error");
   }
   return (
     <Box>
-      <Heading>Admin Product</Heading>
+      <Box display={"flex"} justifyContent="center" alignItems={"center"}>
+      <Heading  h="80px" color={"tomato"} fontSize={"24px"} w="300px">Admin Product Page</Heading>
+
+      </Box>
       <Box>
+        <Flex justifyContent={"center"} alignContent="center" display={"flex"}>
+
+      <Heading h="40px"  color={"gray"} fontSize={"24px"} w="300px">ADD NEW PRODUCT</Heading>
+        </Flex>
+      <Flex flexDirection={"column"} gap="20px" justify={'center'} alignItems="center" >
+
         <Input
+        h="35px"
+        w="500px"
           placeholder="Course Name"
           value={products.course_name}
           type="text"
+          bgColor="lightcyan"
           onChange={(e) => handleChange(e)}
           name="course_name"
         />
-        <Input
+        <Textarea
+          h="35px"
+          w="500px"
           placeholder="Description"
+          bgColor={"lightblue"}
           value={products.description}
           type="text"
           onChange={(e) => handleChange(e)}
           name="description"
         />
         <Input
+          h="35px"
+          w="500px"
           placeholder="Image Url"
           value={products.image}
+          bgColor="lightcyan"
           type="text"
           onChange={(e) => handleChange(e)}
           name="image"
         />
         <Input
+          h="35px"
+          w="500px"
           placeholder="Price"
           value={products.price}
+          bgColor={"lightblue"}
           type="number"
           onChange={(e) => handleChange(e)}
           name="price"
         />
         <Input
+          h="35px"
+          w="500px"
           placeholder="Discount"
+          bgColor="lightcyan"
           value={products.discount}
           type="text"
           onChange={(e) => handleChange(e)}
           name="discount"
         />
-        <Button onClick={() => handlePost(products)}>Submit</Button>
+        <Button colorScheme={"teal"} onClick={() => handlePost(products)}>Submit</Button>
+      </Flex>
       </Box>
-      {loading? "" : data &&
-        data.map((el) => (
-          <Box key={el._id}>
-            <Heading>Course Name : {el.course_name}</Heading>
-            <Text>Description : {el.description}</Text>
-            <Image src={el.image} />
-            <Heading>Price : {el.price}</Heading>
-            <Text>Discount : {el.discount}</Text>
-            <Button onClick={() => handleEdit(el._id)}>Edit in price</Button>
-            <Button onClick={() => handleDelete(el._id)}>
-              Delete this product
-            </Button>
-          </Box>
-        ))}
+
+      {loading
+        ? <>
+        <Skeleton height='40px' mt="20px" >
+        <Box>Hello World!</Box>
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        bg='green.500'
+        mt="20px"
+        color='white'
+        gap={"20px"}
+        fadeDuration={1}
+      >
+      
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        mt="20px"
+        // isLoaded={isLoaded}
+        fadeDuration={4}
+        gap="20px"
+        bg='blue.500'
+        color='white'
+      >
+       
+      </Skeleton>
+      <Skeleton height='40px' mt="20px">
+  
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        bg='green.500'
+        color='white'
+        mt="20px"
+        gap={"20px"}
+        fadeDuration={1}
+      >
+      
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        fadeDuration={4}
+        gap="20px"
+        mt="20px"
+        bg='blue.500'
+        color='white'
+      >
+       
+      </Skeleton>
+      <Skeleton height='40px' mt="20px" >
+        <Box>Hello World!</Box>
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        bg='green.500'
+        color='white'
+        mt="20px"
+        gap={"20px"}
+        fadeDuration={1}
+      >
+      
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        fadeDuration={4}
+        gap="20px"
+        mt="20px"
+        bg='blue.500'
+        color='white'
+      >
+       
+      </Skeleton>
+      <Skeleton height='40px'mt="20px" >
+        <Box>Hello World!</Box>
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        bg='green.500'
+        color='white'
+        mt="20px"
+        gap={"20px"}
+        fadeDuration={1}
+      >
+      
+      </Skeleton>
+      <Skeleton
+        height='40px'
+        // isLoaded={isLoaded}
+        fadeDuration={4}
+        gap="20px"
+        mt="20px"
+        bg='blue.500'
+        color='white'
+      >
+       
+      </Skeleton>
+      </>
+        
+        : <Grid w="full" 
+        templateColumns={{
+          base : "repeat(1,1fr)"
+  ,        md:"repeat(2,1fr)",
+          lg:"repeat(3,1fr)"
+        }}
+        gap="20px"
+        >
+        
+        {data &&
+          data.map((el) => (
+            <GridItem key={el._id} mt="30px">
+              <VStack>
+              <Image borderRadius={"20px"} h="170px" w="300px" src={el.image} />
+              <Heading h="80px" color={"tomato"} fontSize={"24px"} w="300px">{el.course_name}</Heading>
+              <Text fontSize={"16px"} color="darkgray" h="450px" w="400px">{el.description}</Text>
+              <Text fontSize={"24px"} >Price : {el.price}</Text>
+              <Text >Discount : {el.discount}</Text>
+              <Box display={"flex"} gap="20px">
+              <Button colorScheme={"blue"} onClick={() => handleEdit(el._id,el.price)}>Edit Price</Button>
+              <Button colorScheme={"red"} onClick={() => handleDelete(el._id)}>
+                Delete Product
+              </Button>
+
+              </Box>
+
+              </VStack>
+            </GridItem>
+          ))}
+          </Grid>
+          }
+
+
+     
     </Box>
   );
 }
